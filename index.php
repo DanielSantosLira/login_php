69e0c320596a87cdf951860de2b825cc4df89e91
Daniel Santos Pereira Lira <danielsplira@gmail.com>
2023-05-10 21:40:18 -0300

    Update index.php

--- index.php
+++ index.php
@@ -8,43 +8,46 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
     <title>Document</title>
 </head>
+<style>
+    .container{
+        border: solid 1px black;
+        border-radius: 50px;
+    }
+</style>
 
 <body>
-    <div class="container p-5">
+    <div class="container p-5 mb-3">
         <form>
-            <div class="col-10 mx-auto">
-        <div class="mb-3 ">
+
+            <div class="col-8 mx-auto mb-3 ">
                 <label for="nome" class="form-label">Nome</label>
                 <input type="email" class="form-control " id="exampleInputEmail1" aria-describedby="emailHelp">
-                
+
             </div>
-            
-            <div class="mb-3">
+
+            <div class="col-8 mx-auto mb-3">
                 <label for="exampleInputEmail1" class="form-label">Email address</label>
                 <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
-                
+
             </div>
-            <div class="mb-3">
-                <label for="exampleInputEmail1" class="form-label">Telefone</label>
-                <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
-                
-            </div>
-            <div class="mb-3">
+          
+            <div class="col-8 mx-auto mb-3">
                 <label for="exampleInputPassword1" class="form-label">Senha</label>
                 <input type="password" class="form-control" id="exampleInputPassword1">
             </div>
-            <div class="mb-3">
+            <div class="col-8 mx-auto mb-3">
                 <label for="exampleInputPassword1" class="form-label">Confirmar Senha</label>
                 <input type="password" class="form-control" id="exampleInputPassword1">
             </div>
-            
-            
-            </div>
-            <div  class="text-center " >
-            <button type="submit" class="btn btn-primary mg-3 text-center">Submit</button>
 
+
+
+
+            <div class="text-center">
+                <button type="submit" class="btn btn-primary mg-3 text-center">Submit</button>
             </div>
-                        
+
+
         </form>
     </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
